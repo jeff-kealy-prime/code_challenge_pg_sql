2b211a383b8de363daf7effbb5922a87dfbfceea
Jeff Kealy <jeffkealy@yahoo.com>
2016-11-21 14:39:30 -0600

    added database

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,9 @@
 -- Database name
-
+sigma
 -- Document your create tables SQL here
+CREATE TABLE treats (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  description text,
+  pic VARCHAR(100))
+  ;
